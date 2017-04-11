0ffb67d3b2c0d3e61bd2c907a07f61c07443ed7f
xvshiting <597241336@qq.com>
2017-04-11 20:24:30 +0800

    update clean_data

--- clean_data.R
+++ clean_data.R
@@ -21,5 +21,40 @@ JData_Action<-rbind(JData_Action_02,JData_Action_03,JData_Action_04)
 factor_t_numeric<-function(x){
   as.numeric(as.character(x))
 }
+#查看同一User-Id 和 Sku_id 的action
+action_user_skuid<-function(user,sku)
+  {
+  dd<-JData_Action[which(JData_Action$user_id==user&JData_Action$sku_id==sku&JData_Action$type==4),3]
+  JData_Action[which(JData_Action$user_id==user&JData_Action$sku_id==sku&JData_Action$time<=dd),]
+}
+#查看同一User-Id 和 Cate 的action  
+#action_user_skuid(JData_Action.buy$user_id[1000],JData_Action.buy$sku_id[1000])
+#
+action_user_cateid<-function(user,cate)
+{
+  JData_Action[which(JData_Action$user_id==user&JData_Action$cate==cate),]
+}
+#查看同一User 和 Brand 的 action
 #process time (convert to numeric)
+#extra 提取发生过购买行为的用户行为
+JData_User_buy<-unique(JData_Action[JData_Action$type==4,1])
+#发生过购买行为的用户信息
+JData_User_buy.info<-JData_User[!is.na(match(JData_User$user_id,JData_User_buy)),]
+#发生过购买行为的用户 所有行为
+JData_Action.User_Buy<-JData_Action[which(!is.na(match(JData_Action$user_id,JData_User_buy))),]
+#所有购买行为
+JData_Action.buy<-JData_Action[JData_Action$type==4,1]
+
+
+
+
+
+
+
+
+
+
+
+
+
 
